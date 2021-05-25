8faab2a30d192d8cb05c94c9a9aabbe2406d40a3
Mateusz Kmita <mateusz.kmita@mail.com>
2021-05-25 22:52:24 +0200

    Create tests

--- src/test/java/org/mateusz/ioc/AppTest.java
+++ src/test/java/org/mateusz/ioc/AppTest.java
@@ -1,20 +1,82 @@
 package org.mateusz.ioc;
 
-import static org.junit.Assert.assertTrue;
+import org.junit.jupiter.api.Test;
 
-import org.junit.Test;
+import static org.junit.jupiter.api.Assertions.*;
 
 /**
- * Unit test for simple App.
+ * Unit test.
  */
 public class AppTest 
 {
-    /**
-     * Rigorous Test :-)
-     */
     @Test
-    public void shouldAnswerWithTrue()
+    public void shouldReturnObjectOfCorrectType()
     {
-        assertTrue( true );
+        SimpleContainer container = new SimpleContainer();
+        container.registerType(A.class, false);
+
+        assertTrue(container.resolve(A.class) instanceof A);
     }
+
+    @Test
+    public void shouldReturnSubclassOfCorrectType() {
+        SimpleContainer container = new SimpleContainer();
+        container.registerType(A.class, B.class, false);
+
+        assertTrue(container.resolve(A.class) instanceof B);
+    }
+
+    @Test
+    public void shouldReturnUpdatedType() {
+        SimpleContainer container = new SimpleContainer();
+        container.registerType(A.class, B.class, false);
+        container.registerType(A.class, C.class, false);
+        assertTrue(container.resolve(A.class) instanceof C);
+    }
+
+    @Test
+    public void shouldReturnDifferentObjectsForPrototypePolicy() {
+        SimpleContainer container = new SimpleContainer();
+        container.registerType(A.class, B.class, false);
+        assertNotSame(container.resolve(A.class), container.resolve(A.class));
+    }
+
+    @Test
+    public void shouldReturnTheSameObjectForSingletonPolicy() {
+        SimpleContainer container = new SimpleContainer();
+        container.registerType(A.class, B.class, true);
+        assertSame(container.resolve(A.class), container.resolve(A.class));
+    }
+
+    @Test
+    public void shouldThrowExceptionForUnregisteredType() {
+        SimpleContainer container = new SimpleContainer();
+        assertThrows(IllegalArgumentException.class, () -> container.resolve(A.class));
+    }
+
+    @Test
+    public void shouldThrowErrorForNotFoundConstructor() {
+        SimpleContainer container = new SimpleContainer();
+        container.registerType(A.class, D.class, false);
+        assertThrows(Error.class, () -> container.resolve(A.class));
+    }
+}
+
+class A {
+    public A() {
+    }
+}
+
+class B extends A {
+    public B() {
+    }
+}
+
+class C extends A {
+    public C() {
+    }
+}
+
+class D extends A {
+
 }
